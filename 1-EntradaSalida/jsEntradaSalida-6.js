dfad551414b691edcd6987fe37ef914752d6cf62
osc4r20 <47321402+osc4r20@users.noreply.github.com>
2019-02-04 12:26:44 -0300

    Add files via upload
    7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,8 +2,9 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar(){
-    var num1 = document.getElementById('numeroUno').value;
-    var num2 = document.getElementById('numeroDos').value;
-    
+    var num1 = parseInt(document.getElementById('numeroUno').value);
+    var num2 = parseInt(document.getElementById('numeroDos').value);
+    var resultado = num1 + num2;
+    alert("La Suma es "+resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,31 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	
+function sumar(){	
+	var sum1 = parseInt(document.getElementById('numeroUno').value);
+    var sum2 = parseInt(document.getElementById('numeroDos').value);
+    var resultado = sum1 + sum2;
+    alert("La Suma es: "+resultado);
 }
 
-function restar()
-{
-	
+function restar(){
+	var sum1 = parseInt(document.getElementById('numeroUno').value);
+    var sum2 = parseInt(document.getElementById('numeroDos').value);
+    var resultado = sum1 - sum2;
+    alert("La Resta es: "+resultado);
 }
 
-function multiplicar()
-{ 
-	
+function multiplicar(){ 
+	var sum1 = parseInt(document.getElementById('numeroUno').value);
+    var sum2 = parseInt(document.getElementById('numeroDos').value);
+    var resultado = sum1 * sum2;
+    alert("La Multiplicacion es : "+resultado);	
 }
 
-function dividir()
-{
-	
+function dividir(){
+	var sum1 = parseInt(document.getElementById('numeroUno').value);
+    var sum2 = parseInt(document.getElementById('numeroDos').value);
+    var resultado = sum1 / sum2;
+    alert("La Division es: "+resultado);	
 }
 
