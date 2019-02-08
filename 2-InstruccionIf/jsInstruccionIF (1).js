23f6ef12a73f289c9eb9fa975dcfa35518acc14e
osc4r20 <47321402+osc4r20@users.noreply.github.com>
2019-02-08 05:52:31 -0300

    Add files via upload
    3

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,9 @@
-function Mostrar()
-{
+function Mostrar(){
 //tomo la edad  
-
+var edad = document.getElementById("edad").value;
+if (edad =="15"){
+	alert("niña bonita");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
-function Mostrar()
-{
+function Mostrar(){
 //tomo la edad  
-
+	var edad  = document.getElementById("edad").value;
+	if (edad>="18"){
+		alert("Eres mayor de edad");
+	}else if (edad<"18"){
+		alert("Eres menor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
