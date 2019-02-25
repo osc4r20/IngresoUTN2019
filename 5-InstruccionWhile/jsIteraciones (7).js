3b9a402fc80fb0b108caf98f7e38d613cb85cc46
osc4r20 <47321402+osc4r20@users.noreply.github.com>
2019-02-24 22:51:10 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,12 +3,20 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
-	while(){
+	var respuesta="si";
+	var num1;
+	var pregunta;
+	//var pregunta = prompt("Desea continuar");
+	do{
+		num1 = parseInt(prompt("Ingrese un numero"));
 		
-	}
+		acumulador +=num1;
+		pregunta = prompt("Desea continuar");
+		respuesta =pregunta;
+		contador++;
+	}while(respuesta !="no");
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById("suma").value=acumulador;
+document.getElementById("promedio").value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -5,10 +5,18 @@ function Mostrar()
 	var positivo=0;
 	var negativo=1;
 	
-	var respuesta='si';
+	var respuesta="si";
+	do{
+		num1 = parseInt(prompt("Ingrese un numero"));
+		num2 = parseInt(prompt("Ingrese otro numero"));
+		positivo +=num1;
+		negativo *=num2;
+		pregunta = prompt("Desea continuar");
+		respuesta =pregunta;
+		contador++;
+	}while(num2 < 0 &&pregunta !="no");
 
-
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+document.getElementById("suma").value=positivo;
+document.getElementById("producto").value=negativo;
 
 }//FIN DE LA FUNCIÓN
